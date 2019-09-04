88db0105b24d37d0f764054c0d158f88ed2d18e8
PeterLysyk <lyssykpetro@gmail.com>
2019-09-04 03:38:10 +0300

    created all fucntions. removed old test function

--- practiceLPNU/main.cpp
+++ practiceLPNU/main.cpp
@@ -2,10 +2,8 @@
 #include <QApplication>
 #include<QDebug>
 #include <QString>
-#include "createtablesqueries.h"
+#include "tablescreatequeries.h"
 #include"databasefunctions.h"
-
-void addValues(int id, QString firstName, QString lastName, QString birthDate, double weight);
 
 int main(int argc, char *argv[])
 {
@@ -20,48 +18,11 @@ int main(int argc, char *argv[])
     {
         qDebug()<<"Problem with opening DB";
     }
-
-//    QString createTestBaseQuery = "CREATE TABLE TESTBASE ("
-//                    "ID INTEGER,"
-//                    "FIRSTNAME VARCHAR(20),"
-//                    "LASTNAME VARCHAR(20),"
-//                    "BIRTHDATE DATETIME,"
-//                    "WEIGHT DOUBLE);";
-
-//    QSqlQuery createDBQuery;
-
     createAllTablesInTheDataBase(w);
 
-//    if (!createDBQuery.exec(createTestBaseQuery))
-//    {
-//        qDebug()<<"error when creating table";
-//    }
+
     w->getDataBase().close();
     w->show();
 
     return a.exec();
-}
-
-void addValues(int id, QString firstName, QString lastName, QString birthDate, double weight)
-{
-    QSqlQuery qry;
-
-    qry.prepare("INSERT INTO TESTBASE ("
-                "ID,"
-                "FIRSTNAME,"
-                "LASTNAME,"
-                "BIRTHDATE,"
-                "WEIGHT)"
-                "VALUES (?,?,?,?,?);");
-
-    qry.addBindValue(id);
-    qry.addBindValue(firstName);
-    qry.addBindValue(lastName);
-    qry.addBindValue(QDateTime::fromString(birthDate, "mm-dd-yyyy"));
-    qry.addBindValue(weight);
-
-    if (!qry.exec())
-    {
-        qDebug()<<"error creating values to db";
-    }
 }
